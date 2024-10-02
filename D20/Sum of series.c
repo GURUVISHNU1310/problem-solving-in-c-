ef79af6c9ec63106078e6ebb17690fc5d5056894
GURUVISHNU1310 <guruvishnu1310@gmail.com>
2024-10-02 08:42:05 -0700

    Create Sum of series.c

---
+++ D20/Sum of series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+   int n, sum;
+   printf("Enter n value: \n");
+   scanf("%d",&n);
+    sum = 1;
+    printf("%d",sum);
+   for(int i=2; i<=n; i++)
+   {
+     printf("+%d",i);
+     sum += i;
+   }
+   printf("=%d",sum);
+   return 0;
+}
